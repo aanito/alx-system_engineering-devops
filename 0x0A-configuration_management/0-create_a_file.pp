b56b1a32f16bbd7a91cefc2f8ad87e8a69136ab5
root <howmarad@gmail.com>
2023-01-10 10:28:44 -0800

    Create hotfix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 file {  ‘school’ : 
 	path => ‘/tmp/school’,
-	ensure => ‘present’,
+	mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
 	content => ‘I love Puppet’,
-	group => ‘www.data’,
-	owner => ‘www.data’,
-	mode => ‘0744’,
 } 
 
