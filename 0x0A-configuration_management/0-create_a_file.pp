4aae5af9110974719fd45a5f9e795545659af2d8
root <howmarad@gmail.com>
2023-01-10 10:39:40 -0800

    Create a puppet file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-#Create a file with puppet
+# Create a file
 
-
-file { ‘school’: 
-	path    => ‘/tmp/school’,
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => ‘I love Puppet’,
-} 
+file { 'school':
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
