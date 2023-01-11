9276321f4021d08e36fd24c41ea29a5f46ad6051
root <howmarad@gmail.com>
2023-01-11 13:56:49 -0800

    Found the class thing to put initially

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,9 @@
 #Installing the flask
 
-include python
-include python::flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Class['python::flask'],
+class flask {
+  package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Class['python::flask'],
+  }
 }
