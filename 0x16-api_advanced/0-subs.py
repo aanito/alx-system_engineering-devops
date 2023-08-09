da976089be304e5a855f71561185db886431ca4c
aanito <howmarad@gmail.com>
2023-08-09 14:12:37 +0300

    Create the role

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import sys
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers for a given subreddit."""
     headers = {'User-Agent': 'Custom User-Agent/1.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = f'https://www.reddit.com/dev/api/{subreddit}/about.json'
     
     try:
         response = requests.get(url, headers=headers)
