479d3fbf06157faebf34fafecf2dbcaabc5d5859
aanito <howmarad@gmail.com>
2023-08-11 12:35:45 +0300

    Pycodes corrected here as well

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The reddit API requst for the top hot posts of the subreddit 
+The reddit API requst for the top hot posts of the subreddit
 """
 
 import requests
@@ -9,13 +9,13 @@ import requests
 def top_ten(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'MyApp/1.0'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
-        
+
         if posts:
             for post in posts:
                 print(post['data']['title'])
